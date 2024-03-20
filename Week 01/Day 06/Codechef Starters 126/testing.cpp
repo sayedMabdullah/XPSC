d026a1cef582f02f6262b96e25b9707ec97e4ad1
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-03-20 13:25:19 +0600

    testing

---
+++ Week 01/Day 06/Codechef Starters 126/testing.cpp
@@ -0,0 +1,17 @@
+/**
+ * Title: sayed_m_abdullah
+ * Date: "20-03-2024" Time:"13:24:56"
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
