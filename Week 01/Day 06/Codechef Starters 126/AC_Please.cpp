c6b737d45a6f371d9ce2cb334162db2e418d28a2
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-03-20 22:50:28 +0600

    codechef contest and topicwise 2 problem

---
+++ Week 01/Day 06/Codechef Starters 126/AC_Please.cpp
@@ -0,0 +1,27 @@
+/**
+ * Title: sayed_m_abdullah
+ * Date: "20-03-2024" Time:"20:31:42"
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    if (t > 30)
+        cout << "YES"
+             << "\n";
+    else
+        cout << "NO"
+             << "\n";
+
+    return 0;
+}
---
+++ Week 01/Day 06/Codechef Starters 126/Dice_Game_3.cpp
@@ -0,0 +1,24 @@
+/**
+ * Title: sayed_m_abdullah
+ * Date: "20-03-2024" Time:"20:36:07"
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << (n * 6) + (n / 2) << "\n";
+    }
+
+    return 0;
+}
