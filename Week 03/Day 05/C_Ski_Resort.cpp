11017f1d90c641c98153b9724a0022396b0fcd82
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-04-02 20:49:52 +0600

    another one day 5

---
+++ Week 03/Day 05/C_Ski_Resort.cpp
@@ -0,0 +1,73 @@
+/**
+ * Title: sayedMabdullah
+ * Date: "02-04-2024" Time:"17:02:52"
+ * do you have any query ?? sayedmabdullah13@gmail.com
+ *
+ * ---I'm starting in the name of Almighty ALLAH---
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define out cout
+#define in cin
+#define loop for (int i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    ll n, k, q;
+    cin >> n >> k >> q;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    ll r = 0, val = 0, ans = 0;
+
+    while (r < n)
+    {
+        if (v[r] <= q)
+        {
+            val++;
+
+            if (r == n - 1 or v[r + 1] > q)
+            {
+                if (val >= k)
+                {
+                    ll x = val - k + 1;
+                    // ll x = ceil(double(val)/k);
+                    // ans += ceil((x * (x + 1)) / 2);
+                    ans += (x * (x + 1)) / 2;
+                }
+                val = 0;
+            }
+        }
+        r++;
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    in >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /* Alhamdulillah For Everything */
+}
