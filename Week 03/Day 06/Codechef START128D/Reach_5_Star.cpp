3a2aeb20c49977432c623832f109d99b30bab99e
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-04-03 22:32:53 +0600

    day6

---
+++ Week 03/Day 06/Codechef START128D/Reach_5_Star.cpp
@@ -0,0 +1,48 @@
+/**
+ * Title: sayedMabdullah
+ * Date: "03-04-2024" Time:"20:32:08"
+ * do you have any query ?? sayedmabdullah13@gmail.com
+ *
+ * ---I'm starting in the name of Almighty ALLAH---
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define out cout
+#define in cin
+#define loop for (int i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a + b >= 2000)
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    t = 1;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /* Alhamdulillah For Everything */
+}
