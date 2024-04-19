ccefe28325d66508533542d0305ee03a3149cb8a
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-04-19 21:34:13 +0600

    another one

---
+++ Week 05/Day 01/Week04 Unloved Problem Solution/Xorry_1.cpp
@@ -0,0 +1,56 @@
+/**
+ * Title: sayedMabdullah
+ * Date: "19-04-2024" Time:"20:41:45"
+ * do you have any query ?? sayedmabdullah13@gmail.com
+ *
+ * ---I'm starting in the name of Almighty ALLAH---
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define out cout
+#define in cin
+#define loop for (int i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    ll x = n, c = 0;
+    ;
+    while (x != 0)
+    {
+        x /= 2;
+        c++;
+    }
+
+    // cout << c << endl;
+
+    ll a = (1 << c - 1);
+    cout << n - a << " " << a << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    in >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /* Alhamdulillah For Everything */
+}
