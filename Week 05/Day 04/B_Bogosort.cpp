c1890fc643344b56a91ce7e4ec038f3aa3c033b1
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-04-22 22:49:43 +0600

    another one

---
+++ Week 05/Day 04/B_Bogosort.cpp
@@ -0,0 +1,58 @@
+/**
+ * Title: sayedMabdullah
+ * Date: "22-04-2024" Time:"22:34:09"
+ * do you have any query ?? sayedmabdullah13@gmail.com
+ *
+ * ---I'm starting in the name of Almighty ALLAH---
+ **/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define out cout
+#define in cin
+#define loop for (int i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end(), greater<>());
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    in >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+
+    /* Alhamdulillah For Everything */
+}
