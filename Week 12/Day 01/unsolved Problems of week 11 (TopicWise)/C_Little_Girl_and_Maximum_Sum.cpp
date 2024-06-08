d2759f32abd2a669749c3943cbe987af93bb3857
Sayed Mohammad Abdullah <43878226+sayedMabdullah@users.noreply.github.com>
2024-06-08 23:35:22 +0600

    day01 of week12

---
+++ Week 12/Day 01/unsolved Problems of week 11 (TopicWise)/C_Little_Girl_and_Maximum_Sum.cpp
@@ -0,0 +1,158 @@
+/**
+ * ---I'm starting in the name of Almighty ALLAH---
+ *
+ * Date: "08-06-2024"
+ * Time: "22:33:36"
+ *
+ * File: "C_Little_Girl_and_Maximum_Sum.cpp"
+ *
+ **/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define totallyfast                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define int long long
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define Yes cout << "Yes" << nl
+#define no cout << "NO" << nl
+#define No cout << "No" << nl
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// // prime check O(sqer(n))
+// bool isPrime(int n)
+// {
+//     for (int i = 2; i * i <= n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// // sieve algo... prime check O(NLoglogN)
+// vector<bool> isPrime1(int n)
+// {
+//     vector<bool> prm(n + 1, true);
+//     for (int i = 2; i * i <= n; i++)
+//     {
+//         if (prm[i])
+//         {
+//             for (int j = i + i; j <= n; j += i)
+//             {
+//                 prm[j] = false;
+//             }
+//         }
+//     }
+//     return prm;
+// }
+
+// // prime factorizations
+// vector<int> primeFctr(int n)
+// {
+//     vector<int> factor;
+//     map<int, int> mp;
+
+//     for (int i = 2; i * i <= n; i++)
+//     {
+
+//         if (n % i == 0)
+//         {
+//             while (n % i == 0)
+//             {
+//                 mp[i]++;
+//                 n /= i;
+//             }
+//         }
+//     }
+
+//     if (n > 1)
+//         mp[n]++;
+
+//     for (auto x : mp)
+//     {
+//         while (x.second != 0)
+//         {
+//             // cout << x.first << " ";
+//             factor.push_back(x.first);
+//             x.second--;
+//         }
+//     }
+//     return factor;
+// }
+
+// // LCM
+// int lcm(int a, int b)
+// {
+//     return a / __gcd(a, b) * b;
+// }
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> diff_array(n + 2, 0);
+
+    while (m--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        diff_array[l] += 1;
+        diff_array[r + 1] += -1;
+    }
+
+    for (int i = 1; i < n + 2; i++)
+    {
+        diff_array[i] = diff_array[i - 1] + diff_array[i];
+    }
+
+    sort(diff_array.rbegin(), diff_array.rend());
+    sort(a.rbegin(), a.rend());
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += diff_array[i] * a[i];
+    }
+    cout << ans << nl;
+}
+
+signed main()
+{
+    totallyfast;
+
+    int t;
+    t = 1;
+    // cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /**
+     * ___Alhamdulillah For Everything___
+     * do you have any query ? ? sayedmabdullah13 @gmail.com
+     **/
+}
---
+++ Week 12/Day 01/unsolved Problems of week 11 (TopicWise)/Restaurant_Customers.cpp
@@ -0,0 +1,146 @@
+/**
+ * ---I'm starting in the name of Almighty ALLAH---
+ *
+ * Date: "08-06-2024"
+ * Time: "23:10:31"
+ *
+ * File: "Restaurant_Customers.cpp"
+ *
+ **/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define totallyfast                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr)
+#define int long long
+#define nl "\n"
+#define yes cout << "YES" << nl
+#define Yes cout << "Yes" << nl
+#define no cout << "NO" << nl
+#define No cout << "No" << nl
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// // prime check O(sqer(n))
+// bool isPrime(int n)
+// {
+//     for (int i = 2; i * i <= n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// // sieve algo... prime check O(NLoglogN)
+// vector<bool> isPrime1(int n)
+// {
+//     vector<bool> prm(n + 1, true);
+//     for (int i = 2; i * i <= n; i++)
+//     {
+//         if (prm[i])
+//         {
+//             for (int j = i + i; j <= n; j += i)
+//             {
+//                 prm[j] = false;
+//             }
+//         }
+//     }
+//     return prm;
+// }
+
+// // prime factorizations
+// vector<int> primeFctr(int n)
+// {
+//     vector<int> factor;
+//     map<int, int> mp;
+
+//     for (int i = 2; i * i <= n; i++)
+//     {
+
+//         if (n % i == 0)
+//         {
+//             while (n % i == 0)
+//             {
+//                 mp[i]++;
+//                 n /= i;
+//             }
+//         }
+//     }
+
+//     if (n > 1)
+//         mp[n]++;
+
+//     for (auto x : mp)
+//     {
+//         while (x.second != 0)
+//         {
+//             // cout << x.first << " ";
+//             factor.push_back(x.first);
+//             x.second--;
+//         }
+//     }
+//     return factor;
+// }
+
+// // LCM
+// int lcm(int a, int b)
+// {
+//     return a / __gcd(a, b) * b;
+// }
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    map<int, int> mp;
+    while (n--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        mp[l]++;
+        mp[r + 1]--;
+    }
+
+    int ans = 0, sum = 0;
+
+    for (auto [x, y] : mp)
+    {
+        sum += y;
+
+        ans = max(sum, ans);
+    }
+
+    cout << ans;
+}
+
+signed main()
+{
+    totallyfast;
+
+    int t;
+    t = 1;
+    // cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /**
+     * ___Alhamdulillah For Everything___
+     * do you have any query ? ? sayedmabdullah13 @gmail.com
+     **/
+}
